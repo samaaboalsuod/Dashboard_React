c3e40d4214d7b204e4513afac1684d5d4065c434
samaaboalsuod <samaaaboalsuod@gmail.com>
2025-12-22 11:17:42 +0200

    nav responsive done

--- dashboard_react/src/Components/Nav.jsx
+++ dashboard_react/src/Components/Nav.jsx
@@ -1,42 +1,38 @@
-import React, { Component } from 'react';
-import './Nav.css'
-
-import Logo from '../Assets/logo.svg'
-import Notify from '../Assets/notifyIcon.svg'
-import Preview from '../Assets/previewIcon.svg'
-import Profile from '../Assets/profileIcon.svg'
-
+import React from 'react';
+import './Nav.css';
+import Logo from '../Assets/logo.svg';
+import Notify from '../Assets/notifyIcon.svg';
+import Preview from '../Assets/previewIcon.svg';
+import Profile from '../Assets/profileIcon.svg';
 import SearchBar from './SearchBar';
 import NavIcon from './NavIcon';
 
 const Nav = (props) => {
-    return ( <>
-    
-    <nav>
+    return (
+        <nav className="navbar-container">
+            {/* Left Side: Logo (Name is hidden on mobile via CSS) */}
+            <div className='logoPart'>
+                <img className='logo' src={Logo} alt="logo" />
+                <h4 className='userName'>Hi, Samaa</h4>
+            </div>
 
-        <div className='logoPart'>
-            <img className='logo' src={Logo} alt="logo" />
-            <h4>Hi,Samaa</h4>
-        </div>
+            {/* Center: Page Title (Hidden on mobile via CSS) */}
+            <h3 className='pageTitle'>{props.pageTitle}</h3>
 
-        <h3 className='pageTitle'>{props.pageTitle}</h3>
+            {/* Right Side: Search (Hidden on mobile) and Icons (Always visible) */}
+            <div className='navHalf'>
+                <div className='searchWrap'>
+                    <SearchBar SearchText="What do you search for..." />
+                </div>
 
-        <div className='navHalf'>
-
-        <SearchBar SearchText="What do you search for..." />
-
-        <div className='navIconCont'>
-            <NavIcon  navIcon={Notify} />
-            <NavIcon  navIcon={Preview} />
-            <NavIcon  navIcon={Profile} />
-        </div>
-        
-        </div>
-
-    </nav>
-    
-    
-    </> );
+                <div className='navIconCont'>
+                    <NavIcon navIcon={Notify} />
+                    <NavIcon navIcon={Preview} />
+                    <NavIcon navIcon={Profile} />
+                </div>
+            </div>
+        </nav>
+    );
 }
- 
+
 export default Nav;
--- dashboard_react/src/Pages/Categories.jsx
+++ dashboard_react/src/Pages/Categories.jsx
@@ -68,51 +68,6 @@ const Categories = () => {
         icons={[editFill, prevFill, binFill]}
     />
 ))}
-
-                    {/* <TableCard
-                      title="UI/UX Design"
-                      subtitle="Website, web application and all screens design projects"
-                      count={12}
-                      date="21/10/2025"
-                      variant="grey"
-                      icons={[editFill, prevFill, binFill]}
-                    />
-
-                    <TableCard
-                      title="Front End Desing"
-                      subtitle="Website design implementation projects"
-                      count={4}
-                      date="21/10/2025"
-                      variant="transparent"
-                      icons={[editFill, prevFill, binFill]}
-                    />
-
-                    <TableCard
-                      title="Graphic Design"
-                      subtitle="Digital illustrations and Branding projects"
-                      count={5}
-                      date="21/10/2025"
-                      variant="grey"
-                      icons={[editFill, prevFill, binFill]}
-                    />
-
-                    <TableCard
-                      title="Photography"
-                      subtitle="Professionally photos by camera projects"
-                      count={9}
-                      date="21/10/2025"
-                      variant="transparent"
-                      icons={[editFill, prevFill, binFill]}
-                    />
-
-                    <TableCard
-                      title="3D Modeling"
-                      subtitle="3D models created using 3dMax or Blender"
-                      count={3}
-                      date="21/10/2025"
-                      variant="grey"
-                      icons={[editFill, prevFill, binFill]}
-                    /> */}
                 </div>
 
                 <h6>Showing All 5 Categories</h6>
